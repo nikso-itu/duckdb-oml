b39376f345a36b5795e7d97ed97da04723a50c5b
nikso-itu <nikso@itu.dk>
2023-11-06 14:45:32 +0100

    initial TableFunction declaration

--- src/oml_extension.cpp
+++ src/oml_extension.cpp
@@ -1,66 +1,58 @@
 #define DUCKDB_EXTENSION_MAIN
 
 #include "oml_extension.hpp"
+
+#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
 // OpenSSL linked through vcpkg
 #include <openssl/opensslv.h>
 
 namespace duckdb {
 
-inline void OmlScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Oml "+name.GetString()+" 🐥");;
-        });
-}
+inline void OmlPowerConsumptionLoad(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
+    // extract filename
+    string filename = data_p.bind_data->Cast<string>();
 
-inline void OmlOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Oml " + name.GetString() +
-                                                     ", my linked OpenSSL version is " +
-                                                     OPENSSL_VERSION_TEXT );;
-        });
+    // create table if it doesnt exist
+
+    // parse the OML file
+    // insert results into columns of output: output.data[column_id] = data
+    // use SetCardinality on the output to specify the expected number of rows
+    // initialize the the DataChunk with the expected schema (column types)
+    // insert into the output chunk by accessing the vector:
+    // output.data[column].setValue(row, value)
+
+    output.InitializeEmpty(vector<LogicalType>{
+        LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR,
+        LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::FLOAT,
+        LogicalType::FLOAT, LogicalType::FLOAT});
 }
 
 static void LoadInternal(DatabaseInstance &instance) {
-    // Register a scalar function
-    auto oml_scalar_function = ScalarFunction("oml", {LogicalType::VARCHAR}, LogicalType::VARCHAR, OmlScalarFun);
-    ExtensionUtil::RegisterFunction(instance, oml_scalar_function);
-
-    // Register another scalar function
-    auto oml_openssl_version_scalar_function = ScalarFunction("oml_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, OmlOpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, oml_openssl_version_scalar_function);
+    // Register table function
+    auto oml_power_consumption_load = TableFunction("Power_Consumption_load", {LogicalType::VARCHAR}, OmlPowerConsumptionLoad);
+    ExtensionUtil::RegisterFunction(instance, oml_power_consumption_load);
 }
 
-void OmlExtension::Load(DuckDB &db) {
-	LoadInternal(*db.instance);
-}
-std::string OmlExtension::Name() {
-	return "oml";
-}
+void OmlExtension::Load(DuckDB &db) { LoadInternal(*db.instance); }
+std::string OmlExtension::Name() { return "oml"; }
 
 } // namespace duckdb
 
 extern "C" {
 
 DUCKDB_EXTENSION_API void oml_init(duckdb::DatabaseInstance &db) {
-	LoadInternal(db);
+    LoadInternal(db);
 }
 
 DUCKDB_EXTENSION_API const char *oml_version() {
-	return duckdb::DuckDB::LibraryVersion();
+    return duckdb::DuckDB::LibraryVersion();
 }
 }
 
