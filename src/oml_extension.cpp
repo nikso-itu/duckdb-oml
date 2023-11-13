1d1ead8cb40260919b81756508e6ad8b85860b63
nikso <nikso@itu.dk>
2023-11-13 15:10:37 +0100

    parsed values are inserted into table

--- src/oml_extension.cpp
+++ src/oml_extension.cpp
@@ -2,13 +2,13 @@
 
 #include "oml_extension.hpp"
 
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
-
 #include "duckdb.hpp"
 #include "duckdb/catalog/catalog.hpp"
+#include "duckdb/catalog/catalog_entry/table_catalog_entry.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
 #include "duckdb/function/scalar_function.hpp"
+#include "duckdb/main/appender.hpp"
 #include "duckdb/main/extension_util.hpp"
 #include "duckdb/parser/column_definition.hpp"
 #include "duckdb/parser/constraints/not_null_constraint.hpp"
@@ -17,6 +17,30 @@
 #include <iostream>
 
 namespace duckdb {
+
+struct oml_append_information {
+    duckdb::unique_ptr<InternalAppender> appender;
+};
+
+void append_oml_chunk(oml_append_information &info, DataChunk &data) {
+    auto &append_info = info.appender;
+    append_info->AppendDataChunk(data);
+}
+
+void AppendData(ClientContext &context, TableFunctionInput &data_p, DataChunk &data) {
+    auto &bind_data = data_p.bind_data->CastNoConst<BaseOMLData>();
+    auto &catalog = Catalog::GetCatalog(context, bind_data.catalog);
+
+    auto &tbl_catalog = catalog.GetEntry<TableCatalogEntry>(context, bind_data.schema, bind_data.table);
+    auto append_info = make_uniq<oml_append_information>();
+    append_info->appender = make_uniq<InternalAppender>(context, tbl_catalog);
+
+    append_oml_chunk(*append_info, data);
+
+    // Flush any incomplete chunks
+    append_info->appender->Flush();
+    append_info->appender.reset();
+}
 
 void CreateTable(ClientContext &context, BaseOMLData &bind_data) {
     auto info = make_uniq<CreateTableInfo>();
@@ -49,15 +73,18 @@ inline unique_ptr<FunctionData> OmlLoadBind(ClientContext &context, TableFunctio
     result->schema = "main";
     result->table = "Power_Consumption";
 
-    // expected output schema (column types)
-    return_types = {LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR,
-                    LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::FLOAT,
-                    LogicalType::FLOAT, LogicalType::FLOAT};
-    names = {"experiment_id", "node_id", "node_id_seq", "time_sec", "time_usec", "power", "current", "voltage"};
-
-    result->column_names = names;
-    result->column_types = return_types;
+    // define types for data chunk
+    vector<LogicalType> column_types = {LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR,
+                                        LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::FLOAT,
+                                        LogicalType::FLOAT, LogicalType::FLOAT};
+    vector<string> column_names = {"experiment_id", "node_id", "node_id_seq", "time_sec", "time_usec", "power", "current", "voltage"};
+    result->column_types = column_types;
+    result->column_names = column_names;
     result->not_null_constraint = {false, false, false, true, true, true, true, true};
+
+    // define output column names and types
+    return_types = column_types;
+    names = column_names;
 
     return std::move(result);
 }
@@ -89,6 +116,10 @@ inline void OmlLoad(ClientContext &context, TableFunctionInput &data_p, DataChun
         }
     }
 
+    // initialize data chunk for saving parsed oml data
+    DataChunk chunk;
+    chunk.Initialize(context, bind_data.column_types);
+
     idx_t row_count = 0;
     while (std::getline(file, line)) {
         std::istringstream iss(line);    // split line
@@ -100,24 +131,33 @@ inline void OmlLoad(ClientContext &context, TableFunctionInput &data_p, DataChun
         }
 
         if (fields.size() == 8) {
-            // insert field values into output
-            output.SetValue(0, row_count, Value(fields[0]));
-            output.SetValue(1, row_count, Value(fields[1]));
-            output.SetValue(2, row_count, Value(fields[2]));
-            output.SetValue(3, row_count, Value(fields[3]));
-            output.SetValue(4, row_count, Value(fields[4]));
-            output.SetValue(5, row_count, Value::FLOAT(std::stof(fields[5])));
-            output.SetValue(6, row_count, Value::FLOAT(std::stof(fields[6])));
-            output.SetValue(7, row_count, Value::FLOAT(std::stof(fields[7])));
+            // insert field values into data chunk
+            idx_t varchar_amount = 5;
+            idx_t float_amount = 3;
+
+            // insert VARCHAR's
+            for (idx_t i = 0; i < varchar_amount; i++) {
+                chunk.SetValue(i, row_count, Value(fields[i]));
+                output.SetValue(i, row_count, Value(fields[i]));
+            }
+
+            // insert FLOAT's
+            for (idx_t i = varchar_amount; i < (varchar_amount + float_amount); i++) {
+                chunk.SetValue(i, row_count, Value::FLOAT(std::stof(fields[i])));
+                output.SetValue(i, row_count, Value::FLOAT(std::stof(fields[i])));
+            }
 
             // increment row count
             row_count++;
-
-            // insert values into table
         }
     }
 
+    chunk.SetCardinality(row_count);
     output.SetCardinality(row_count);
+
+    // insert values into table
+    AppendData(context, data_p, chunk);
+
     bind_data.finished_reading = true;
     file.close();
 }
